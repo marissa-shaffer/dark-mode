8fa4371faf240a6a04a35be8bd0a474b4250fbc6
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-04-19 13:22:38 -0500

    Added class to body. Set up Effect hook.

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -3,6 +3,14 @@ import React, { useEffect } from 'react';
 
 export const useDarkMode = (key, initialValues) => {
     const [darkMode, setDarkMode] = useLocalStorage([key], initialValues);
+
+    useEffect(() => {
+        if (darkMode === true) {
+            document.body.classList.add("dark-mode");
+        } else {
+            document.body.classList.remove("dark-mode");
+            }
+    }, [darkMode]); 
 }
 
 export default useDarkMode;
